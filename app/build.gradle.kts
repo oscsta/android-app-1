018079278eae2db02b094c097be216ab8e2beadc
oscsta <oscstal@gmail.com>
2025-06-29 10:37:59 +0000

    basic manifest in place

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,13 @@ android {
         applicationId = "com.github.oscsta.runni"
         minSdk = 31
         targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
     }
+}
+
+kotlin {
+    jvmToolchain(24)
 }
 
 dependencies {
