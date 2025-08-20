5c7b6ddbee50b08740fe4e97a4159dd6ec11195a
OscSta <oscstal@gmail.com>
2025-08-20 21:05:03 +0200

    ADd extra large label

--- app/src/main/kotlin/com/github/oscsta/runni/ui/Type.kt
+++ app/src/main/kotlin/com/github/oscsta/runni/ui/Type.kt
@@ -1,5 +1,13 @@
 package com.github.oscsta.runni.ui
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
+val Typography.labelExtraLarge: TextStyle
+    get() = TextStyle(
+        fontSize = 18.sp,
+        fontWeight = FontWeight.Bold
+    )
 val AppTypography = Typography()
